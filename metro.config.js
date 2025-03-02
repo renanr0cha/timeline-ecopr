e63f8bfde6ed0cbe5b3409ad80b69dca5c336672
Renan <renan.aragorn@gmail.com>
2025-03-02 00:39:54 -0400

    chore: update dependencies and configure NativeWind

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
+// Get the default configuration for Expo
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// Export the config with NativeWind support
+module.exports = withNativeWind(config, {
+  input: './global.css',
+});
